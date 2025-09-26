417ebc525fbbc0752202dc93d9ac00aad6857753
sb-111 <117798778+sb-111@users.noreply.github.com>
2025-09-27 03:06:34 +0900

    feat: Octree 생성 로직 수정
    1. UOctree의 공간 자료형을 FBox에서 FBound로 변경
    2. 재귀적 생성 로직 수정
    
    TODO: 쿼리 로직 수정 필요

--- TL2/Octree.cpp
+++ TL2/Octree.cpp
@@ -11,43 +11,82 @@ UOctree::~UOctree()
     Release();
 }
 
-void UOctree::Initialize(const FBox& InBounds)
+void UOctree::Initialize(const FBound& InBounds)
 {
     Root = new FOctreeNode(InBounds);
 }
 
-void UOctree::Build(const TArray<AActor*>& InActors, int32 Depth)
+void UOctree::Build(const TArray<AActor*>& InActors, FBound& WorldBounds, int32 Depth)
 {
+    // 기존 트리가 있다면 정리
     if (Root)
     {
-        ReleaseRecursive(Root);
-        Root = nullptr;
+        Release();
     }
+    Root = BuildRecursive(InActors, WorldBounds, Depth); // Depth 기본값 0 
 
     // 월드 전체 Bounds는 외부에서 넘겨준다고 가정
     //Root = BuildRecursive(InActors, InActors.Num() > 0 ? InActors[0]->GetComponentsBoundingBox() : FBox(EForceInit::ForceInit), 0);
 }
 
-FOctreeNode* UOctree::BuildRecursive(const TArray<AActor*>& InActors, const FBox& Bounds, int32 Depth)
+FOctreeNode* UOctree::BuildRecursive(const TArray<AActor*>& InActors, const FBound& Bounds, int32 Depth)
 {
+    // 해당 Bound에 들어올 액터가 없으면 자식 노드 생성할 필요 없음
     if (InActors.Num() == 0) return nullptr;
 
     FOctreeNode* Node = new FOctreeNode(Bounds);
+    Node->Actors = InActors; // 일단 액터들을 현재 노드의 액터 배열에 할당
+
+    // 분할 종료 조건(해당 노드가 Leaf) = 최대 깊이 도달 or 액터 수가 최대치 이하면 잎 노드 확정
+    if (Depth >= MaxDepth || Node->Actors.Num() <= MaxActorsPerNode) return Node;
+
+    // 분할 로직
+    FVector Center = Node->Bounds.GetCenter();
+    FVector HalfExtent = Node->Bounds.GetExtent() * 0.5f;
+
+    FBound ChildBounds[8];
+    for (int32 i = 0; i < 8;++i)
+    {
+        FVector ChildCenter = Center;
+        ChildCenter.X += (i & 1 ? HalfExtent.X : -HalfExtent.X);
+        ChildCenter.Y += (i & 2 ? HalfExtent.Y : -HalfExtent.Y);
+        ChildCenter.Z += (i & 4 ? HalfExtent.Z : -HalfExtent.Z);
+        ChildBounds[i] = FBound(ChildCenter - HalfExtent, ChildCenter + HalfExtent);
+    }
 
     // 이 노드 안에 들어가는 Actor 모으기
-    for (AActor* Actor : InActors)
+    TArray<AActor*> ChildActors[8];
+    for (AActor* Actor : Node->Actors)
     {
+        FVector ActorLocation = Actor->GetActorLocation();
+        for (int32 i = 0; i < 8; ++i)
+        {
+            // i번째 자식 공간에 있으면
+            if (ChildBounds[i].IsInside(ActorLocation))
+            {
+                ChildActors[i].Add(Actor);
+                break;
+            }
+        }
       /*  if (Bounds.IsInsideOrOn(Actor->GetActorLocation()))
         {
             Node->Actors.Add(Actor);
         }*/
     }
 
-    // 종료 조건
-    if (Depth >= MaxDepth || Node->Actors.Num() <= MinActorsPerNode)
+    // 분할 되었으므로, 현재 노드는 액터를 소유 X
+    Node->Actors.Empty();
+
+    for (int32 i = 0; i < 8; ++i)
     {
-        return Node;
+        if (ChildActors[i].Num() > 0)
+        {
+            // 노드 자식들에 대해 재귀적으로 생성
+            Node->Children[i] = BuildRecursive(ChildActors[i], ChildBounds[i], Depth + 1);
+        }
     }
+
+    return Node;
 
     // 8개 자식 영역으로 분할
    /* FVector Center = Bounds.GetCenter();
@@ -64,7 +103,7 @@ FOctreeNode* UOctree::BuildRecursive(const TArray<AActor*>& InActors, const FBox
         Node->Children[i] = BuildRecursive(Node->Actors, ChildBounds, Depth + 1);
     }*/
 
-    return Node;
+    //return Node;
 }
 
 void UOctree::Query(const FRay& Ray, TArray<AActor*>& OutActors) const
--- TL2/Octree.h
+++ TL2/Octree.h
@@ -5,13 +5,13 @@
 
 struct FOctreeNode
 {
-    FBox Bounds;                       // 이 노드의 공간 범위
+    FBound Bounds;                       // 이 노드의 공간 범위
     TArray<AActor*> Actors;            // 이 범위 안의 Actor들
     FOctreeNode* Children[8] = { nullptr }; // 자식 노드 (최대 8개)
     
     UAABoundingBoxComponent* AABoundingBoxComponent;
 
-    FOctreeNode(const FBox& InBounds)
+    FOctreeNode(const FBound& InBounds)
         : Bounds(InBounds)
     {
         AABoundingBoxComponent = NewObject<UAABoundingBoxComponent>();
@@ -27,9 +27,9 @@ public:
     DECLARE_CLASS(UOctree, UObject)
     UOctree();
     ~UOctree();
-    void Initialize(const FBox& InBounds);
+    void Initialize(const FBound& InBounds);
     // 옥트리 빌드
-    void Build(const TArray<AActor*>& InActors, int32 Depth = 0);
+    void Build(const TArray<AActor*>& InActors, FBound& WorldBounds, int32 Depth = 0);
 
     // 레이 기반 Actor 검색
     void Query(const FRay& Ray, TArray<AActor*>& OutActors) const;
@@ -41,10 +41,17 @@ public:
 private:
     FOctreeNode* Root = nullptr;
     int32 MaxDepth = 5;//최대 깊이 조절해봐야하고 
-    int32 MinActorsPerNode = 5;//노드당 최소 엑터수 이거 이하여야만 끝나는 거 
+    // Leaf Node에 존재할 수 있는 최대 액터 수
+    int32 MaxActorsPerNode = 5;//노드당 최소 엑터수 이거 이하여야만 끝나는 거 
 
     // 내부 함수들
-    FOctreeNode* BuildRecursive(const TArray<AActor*>& InActors, const FBox& Bounds, int32 Depth);
+    /**
+    * @brief 자식 노드를 재귀적으로 생성합니다.
+    * @param InActors 액터 배열
+    * @param Bounds 해당 노드가 가진 범위
+    * @param Depth 트리의 현재 깊이
+    */
+    FOctreeNode* BuildRecursive(const TArray<AActor*>& InActors, const FBound& Bounds, int32 Depth);
     void QueryRecursive(const FRay& Ray, FOctreeNode* Node, TArray<AActor*>& OutActors) const;
     void ReleaseRecursive(FOctreeNode* Node);
 };
