e7626e76e5c5b8d3634222f3e7c96e931ac3cc84
changgeun <hoi000115@gmail.com>
2025-09-30 22:38:28 +0900

    feat: Engine 기반 작업

---
+++ TL2/Engine.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "Object.h"
+#
+#include "World.h"
+
+enum class EWorldType
+{
+    None,
+    Editor,
+    PIE,
+    Game
+};
+
+struct FWorldContext
+{
+    EWorldType WorldType = EWorldType::None;
+    UWorld* CurrentWorld = nullptr;
+
+    UWorld* World() { return CurrentWorld; }
+    const UWorld* World() const { return CurrentWorld; }
+
+    void SetWorld(UWorld* InWorld, EWorldType InType)
+    {
+        CurrentWorld = InWorld;
+        WorldType = InType;
+    }
+class UEngine :
+    public UObject
+{
+public:
+    DECLARE_CLASS(UEngine,UObject)
+    
+    TArray<FWorldContext> WorldContexts;
+
+    UWorld* GetWorld(EWorldType Type);
+};
+
