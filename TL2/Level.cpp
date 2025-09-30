ad929e69280f1949a69a5802895eeea4137ee1f1
sb-111 <117798778+sb-111@users.noreply.github.com>
2025-09-30 23:49:16 +0900

    feat: ULevel 추가

---
+++ TL2/Level.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "Level.h"
+
+ULevel::ULevel()
+{
+}
+
+ULevel::~ULevel()
+{
+	for (int i = Actors.Num() - 1; i >= 0; --i)
+	{
+		if (Actors[i] != nullptr)
+		{
+			//delete Actors[i]; 
+			Actors[i] = nullptr;
+		}
+	}
+	Actors.Empty();
+}
+
+void ULevel::AddActor(AActor* InActor)
+{
+	if (InActor)	
+	{
+		Actors.Add(InActor);
+	}
+}
+
+void ULevel::RemoveActor(AActor* InActor)
+{
+	if (InActor)
+	{
+		Actors.Remove(InActor);
+		//delete InActor;
+	}
+}
+
+const TArray<AActor*>& ULevel::GetActors() const
+{
+	return Actors;
+}
