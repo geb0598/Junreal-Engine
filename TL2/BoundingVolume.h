b77e0fadb4b02d686e2fbf11673b60b9533bbe98
homing123 <k99960942@gmail.com>
2025-10-11 11:21:23 +0900

    aabb,obb일단 옮김

---
+++ TL2/BoundingVolume.h
@@ -0,0 +1,107 @@
+﻿#pragma once
+#include "Vector.h"
+
+// Unreal-style simple ray type
+// 이거 어디둘지 모르겠음
+struct alignas(16) FRay
+{
+    FVector Origin;
+    FVector Direction; // Normalized
+};
+
+struct FAABB
+{
+    FVector Min;
+    FVector Max;
+
+    FAABB& operator+=(const FAABB& Other)
+    {
+        Min.X = std::min(Min.X, Other.Min.X);
+        Min.Y = std::min(Min.Y, Other.Min.Y);
+        Min.Z = std::min(Min.Z, Other.Min.Z);
+
+        Max.X = std::max(Max.X, Other.Max.X);
+        Max.Y = std::max(Max.Y, Other.Max.Y);
+        Max.Z = std::max(Max.Z, Other.Max.Z);
+
+        return *this;
+    }
+
+    // 센터 반환
+    FVector GetCenter() const
+    {
+        return (Min + Max) * 0.5f;
+    }
+    // 박스의 절반 크기
+    FVector GetExtent() const
+    {
+        return (Max - Min) * 0.5f;
+    }
+
+    // 표면적 계산 (SAH용)
+    float GetSurfaceArea() const
+    {
+        FVector size = Max - Min;
+        if (size.X <= 0.0f || size.Y <= 0.0f || size.Z <= 0.0f) return 0.0f;
+        return 2.0f * (size.X * size.Y + size.Y * size.Z + size.Z * size.X);
+    }
+    bool IsInside(const FVector& Point) const
+    {
+        return (Point.X >= Min.X && Point.X <= Max.X &&
+            Point.Y >= Min.Y && Point.Y <= Max.Y &&
+            Point.Z >= Min.Z && Point.Z <= Max.Z);
+    }
+    bool IsIntersect(const FAABB& Other) const
+    {
+        return (Min.X <= Other.Max.X && Max.X >= Other.Min.X &&
+            Min.Y <= Other.Max.Y && Max.Y >= Other.Min.Y &&
+            Min.Z <= Other.Max.Z && Max.Z >= Other.Min.Z);
+    }
+};
+
+struct FOBB
+{
+    FVector Center;
+    FVector Axis[3]; //Forward, Right, Up
+    FVector Extents; //half size (x(Forward), y(Right), z(Up))
+
+    //AABB + WorldMatrix 생성자
+    FOBB(const FAABB& LocalAABB, const FTransform& WorldTransform)
+    {
+        LocalAABBWorldTransformCalc(LocalAABB, WorldTransform);
+    }
+    void LocalAABBWorldTransformCalc(const FAABB& LocalAABB, const FTransform& WorldTransform)
+    {
+        FVector LocalAABBCenter = LocalAABB.GetCenter();
+        FVector LocalAABBExtent = LocalAABB.GetExtent();
+        FVector LocalAABBAxis[3] = { FVector(1,0,0),FVector(0,1,0),FVector(0,0,1) };
+
+        FMatrix WorldMatrix = WorldTransform.ToMatrixWithScaleLocalXYZ();
+        FVector4 WorldCenter = FVector4(LocalAABBCenter, 1) * WorldMatrix;
+        FVector4 WorldExtent = FVector4(LocalAABBExtent, 1) * WorldMatrix;
+        FVector4 WorldAxis[3];
+        WorldAxis[0] = FVector4(LocalAABBAxis[0], 1) * WorldMatrix;
+        WorldAxis[1] = FVector4(LocalAABBAxis[1], 1) * WorldMatrix;
+        WorldAxis[2] = FVector4(LocalAABBAxis[2], 1) * WorldMatrix;
+        Center = FVector(WorldCenter.X, WorldCenter.Y, WorldCenter.Z);
+        Extents = FVector(WorldExtent.X, WorldExtent.Y, WorldExtent.Z);
+        Axis[0] = FVector(LocalAABBAxis[0].X, LocalAABBAxis[0].Y, LocalAABBAxis[0].Z);
+        Axis[1] = FVector(LocalAABBAxis[1].X, LocalAABBAxis[1].Y, LocalAABBAxis[1].Z);
+        Axis[2] = FVector(LocalAABBAxis[2].X, LocalAABBAxis[2].Y, LocalAABBAxis[2].Z);
+    }
+
+    FVector GetVertex(uint32 idx) const
+    {
+        idx = idx % 8;
+        FVector Vertex = Center;
+        Vertex += (idx & 1) == 0 ? Axis[0] * Extents.X : -Axis[0] * Extents.X;
+        Vertex += (idx & (1 << 1)) == 0 ? Axis[1] * Extents.X : -Axis[1] * Extents.X;
+        Vertex += (idx & (1 << 2)) == 0 ? Axis[2] * Extents.X : -Axis[2] * Extents.X;
+        return Vertex;
+    }
+};
+
+
+bool IntersectRayAABB(const FRay& Ray, const FAABB& AABB, float& OutDistance);
+
+bool IntersectOBBAABB(const FOBB& OBB, const FAABB& AABB);
