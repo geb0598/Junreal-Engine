3811f3104b9c1b90f16c31431641493a08c7197e
changgeun <hoi000115@gmail.com>
2025-10-13 06:27:05 +0900

    feat: DecalActor 오류 수정

--- TL2/DecalActor.cpp
+++ TL2/DecalActor.cpp
@@ -1,12 +1,21 @@
 ﻿// DecalActor.cpp
 #include "pch.h"
 #include "DecalActor.h"
+#include "ObjectFactory.h"
 
 ADecalActor::ADecalActor()
 {
-    // DecalComponent 생성 및 루트로 설정
+    // 기존 RootComponent 제거 후 DecalComponent를 Root로 설정
+    if (RootComponent)
+    {
+        USceneComponent* TempRootComponent = RootComponent;
+        RootComponent = nullptr;
+        DeleteComponent(TempRootComponent);
+    }
+
     DecalComponent = CreateDefaultSubobject<UDecalComponent>(FName("DecalComponent"));
-    DecalComponent->SetupAttachment(RootComponent);
+    RootComponent = DecalComponent;
+    AddComponent(DecalComponent);
 }
 
 ADecalActor::~ADecalActor()
@@ -44,20 +53,41 @@ bool ADecalActor::DeleteComponent(USceneComponent* ComponentToDelete)
 
 UObject* ADecalActor::Duplicate()
 {
-    ADecalActor* NewActor = static_cast<ADecalActor*>(AActor::Duplicate());
-    if (NewActor)
+    // 원본(this)의 컴포넌트들 저장
+    USceneComponent* OriginalRoot = this->RootComponent;
+
+    // 얕은 복사 수행 (생성자 실행됨 - DecalComponent 생성)
+    ADecalActor* DuplicatedActor = NewObject<ADecalActor>(*this);
+
+    // 생성자가 만든 컴포넌트 삭제
+    if (DuplicatedActor->DecalComponent)
     {
-        // DecalComponent는 부모의 Duplicate에서 처리됨
-        NewActor->DecalComponent = static_cast<UDecalComponent*>(NewActor->GetRootComponent());
+        DuplicatedActor->OwnedComponents.Remove(DuplicatedActor->DecalComponent);
+        ObjectFactory::DeleteObject(DuplicatedActor->DecalComponent);
+        DuplicatedActor->DecalComponent = nullptr;
     }
-    return NewActor;
+
+    DuplicatedActor->RootComponent = nullptr;
+    DuplicatedActor->OwnedComponents.clear();
+
+    // 원본의 RootComponent(DecalComponent) 복제
+    if (OriginalRoot)
+    {
+        DuplicatedActor->RootComponent = Cast<USceneComponent>(OriginalRoot->Duplicate());
+    }
+
+    // OwnedComponents 재구성 및 타입별 포인터 재설정
+    DuplicatedActor->DuplicateSubObjects();
+
+    return DuplicatedActor;
 }
 
 void ADecalActor::DuplicateSubObjects()
 {
-    AActor::DuplicateSubObjects();
+    // Duplicate()에서 이미 RootComponent를 복제했으므로
+    // 부모 클래스가 OwnedComponents를 재구성
+    Super_t::DuplicateSubObjects();
 
-    // DecalComponent 재설정
+    // 타입별 포인터 재설정
     DecalComponent = Cast<UDecalComponent>(RootComponent);
-
 }
