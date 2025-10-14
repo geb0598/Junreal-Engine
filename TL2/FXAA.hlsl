3b209e6745354c322c76769b81a461c305f093b5
homing123 <k99960942@gmail.com>
2025-10-14 16:28:34 +0900

    일단 셰이더  작성 멈춤

--- TL2/FXAA.hlsl
+++ TL2/FXAA.hlsl
@@ -3,6 +3,8 @@
 float3 Lumaniance = { 0.299f, 0.587f, 0.114 };
 float ContrastThreshold = 0.0312f;
 float RelativeThreshold = 0.063f;
+
+float FXAAStrength = 1.0f;
 //Get Texture Info
 //uint OutTexWidth, OutTexHeight, OutMipCount = 0;
 //uint InMipLevel = 0;
@@ -10,7 +12,7 @@ float RelativeThreshold = 0.063f;
 
 float GetFrameSample(float2 uv)
 {
-    return dot(FrameColor.Sample(PointSampler, uv))
+    return dot(FrameColor.Sample(PointSampler, uv).rgb, Lumaniance);
 }
 
 float4 mainPS(PS_Input i) : SV_TARGET
@@ -20,8 +22,10 @@ float4 mainPS(PS_Input i) : SV_TARGET
     FrameColor.GetDimensions(0, TexWidth, TexHeight, MipCount);
     float2 TexSizeRCP = float2(1 / (float) TexWidth, 1 / (float) TexHeight);
     
+    float3 Color = FrameColor.Sample(PointSampler, i.uv).rgb;
+    
     //NearPixel Sample
-    float M = GetFrameSample(i.uv + int2( 0,  0) * TexSizeRCP);
+    float M = dot(Color, Lumaniance);
     float N = GetFrameSample(i.uv + int2( 0,  1) * TexSizeRCP);
     float S = GetFrameSample(i.uv + int2( 0, -1) * TexSizeRCP);
     float E = GetFrameSample(i.uv + int2( 1,  0) * TexSizeRCP);
@@ -49,12 +53,35 @@ float4 mainPS(PS_Input i) : SV_TARGET
     float SW = GetFrameSample(i.uv + int2(-1, -1) * TexSizeRCP);
     
     float filter = (2 * (N + S + E + W) + (NE + NW + SE + SW)) / 12.0f;
-    
     float BlendFactor = saturate(abs(filter - M) / Contrast);
-    
     BlendFactor = smoothstep(0, 1, BlendFactor);
     
+    float Horizontal = 2 * abs(N + S - 2 * M) + abs(NE + SE - 2 * E) + abs(NW + SW - 2 * W); //가장자리 선 모양이 수평이라는 뜻
+    float Vertical = 2 * abs(E + W - 2 * M) + abs(NE + NW - 2 * N) + abs(SE + SW - 2 * S);
+    bool bHorizontal = Horizontal > Vertical;
     
+    float PLuminance = bHorizontal ? N : E; //양의방향
+    float NLuminance = bHorizontal ? S : W; //음의방향
+    float PGradient = abs(PLuminance - M);
+    float NGradient = abs(NLuminance - M);
+    
+    float2 PixelUVStep = bHorizontal ? float2(0, TexSizeRCP.y) : float2(TexSizeRCP.x, 0);
+    
+    //양의방향과 음의방향 중 어디가 가장자리의 바깥부분인지에 따라 PixelUVStep 방향이 바뀐다.
+    PixelUVStep = PGradient > NGradient ? PixelUVStep : -PixelUVStep;
+    
+    
+    //엣지가 끝나는지점과의 거리 측정
+    float2 CurUV = i.uv;
+    
+    
+    
+    
+    
+    float2 BlendUV = i.uv + (bHorizontal ? float2(0, 1) : float2(1, 0)) * PixelUVStep * BlendFactor;
+    float3 BlendColor = FrameColor.Sample(LinearSampler, BlendUV).rgb;
+    
+    return float4(Color.rrr, 1);
 }
 
 
